ab5c34a4b58cef873dec7cd786a0c2f643b0d404
phainestha1 <250wons@gmail.com>
2021-09-22 19:39:07 +0900

    doit!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 import App from './components/App';
 import "./style/index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
+
+// React.StrictMode
